b3843610f02c0667336122f06cb837a2afa1db7c
Takashi SASAKI <takashi316@gmail.com>
2026-07-29 15:59:13 +0900

    Use ImagineDeck generation v45

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@
 // Every imported script URL is versioned because some portal entry points use
 // the browser default updateViaCache policy for Service Worker imports.
 importScripts('/sw-core-v34.js');
-importScripts('/sw-imaginedeck-generation-v43.js');
+importScripts('/sw-imaginedeck-generation-v45.js');
 importScripts('/sw-imaginedeck-network-v36.js');
 importScripts('/sw-imaginedeck-refresh-timeout-v43.js');
 importScripts('/sw-imaginedeck-navigation-retry-v44.js');
