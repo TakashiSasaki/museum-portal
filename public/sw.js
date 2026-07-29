e74cfc98a6b3dca27551397a1ec0ee086d7e89c0
Takashi SASAKI <takashi316@gmail.com>
2026-07-29 14:36:43 +0900

    fix(imaginedeck): load timeout budget v41

--- public/sw.js
+++ public/sw.js
@@ -4,4 +4,4 @@
 importScripts('/sw-core-v34.js');
 importScripts('/sw-imaginedeck-generation-v36.js');
 importScripts('/sw-imaginedeck-network-v36.js');
-importScripts('/sw-imaginedeck-refresh-timeout-v40.js');
+importScripts('/sw-imaginedeck-refresh-timeout-v41.js');
